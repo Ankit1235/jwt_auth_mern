e1b8a84b7cae0935eee2e79276e39544a1a976cf
Ankit <ankit.dharse138@gmail.com>
2024-07-29 02:27:42 -0700

    connect mongodb with mongoose ORM

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,20 @@
 import express from "express";
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-const PORT = 5500;
 
-app.listen(PORT, () =>
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING as string)
+.then(() => console.log("Connected to MongoDB"))
+.catch((error) => console.error("Error connecting to MongoDB:", error));
+
+app.listen(process.env.PORT, () =>
 {
-    console.log("Server is listeing at PORT", PORT);
+    console.log("Server is listeing at PORT", process.env.PORT);
 });
-
 
